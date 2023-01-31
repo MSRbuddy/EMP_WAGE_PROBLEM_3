73492139ccec0874d612c2bac1d0ee906bbfde23
MSRbuddy <meghanadasari222@gmail.com>
2023-01-31 19:45:54 +0530

    Welcome to Employee Computation Problem : Part-3

---
+++ EmpWagePrblm.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EmpWagePrblm3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to employee computation problem3...!");
+        }
+    }
+}
