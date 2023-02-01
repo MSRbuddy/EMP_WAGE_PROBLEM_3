9956d7998e1b4e26310eba844079f0ed944eeeb7
MSRbuddy <122597457+MSRbuddy@users.noreply.github.com>
2023-02-01 23:07:26 +0530

    Added : Line Comparison Problem

---
+++ LC-UC1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LC_UC1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2;
+            x1 = 3;
+            x2 = 4;
+            y1 = 4;
+            y2 = 3;
+            var distance = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine(distance);
+        }
+    }
+}
---
+++ LC-UC4.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace LC_UC4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, y1, y2;
+            x1 = 1; x2 = 2; y1 = 3; y2 = 4;
+
+            var L1 = Math.Sqrt((Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2)));
+            Console.WriteLine("Length of Line 1 is: " + L1);
+
+            var L2 = Math.Sqrt((Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2)));
+            Console.WriteLine("Length of Line 2 is: " + L2);
+
+            if(L1.CompareTo(L2) == 0)
+            {
+               Console.WriteLine("Length of Line 1 is Equals to Length of Line 2");
+            }
+            if (L1.CompareTo(L2) != 0)
+
+            {
+                Console.WriteLine("Length of Line 1 is Not Equals to Length of Line 2");
+            }
+        }
+    }
+}
